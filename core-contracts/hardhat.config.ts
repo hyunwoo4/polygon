7f657e8a2d36e7ebda4d57e69040ba7c1ecef62e
ohbyeongmin <fbottlem@gmail.com>
2024-01-11 11:28:22 +0900

    add core-contracts

---
+++ core-contracts/hardhat.config.ts
@@ -0,0 +1,76 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import "@primitivefi/hardhat-dodoc";
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-deploy";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 115,
+          },
+          outputSelection: {
+            "*": {
+              "*": ["storageLayout"],
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    root: {
+      url: process.env.ROOT_RPC || "",
+      accounts: process.env.ROOT_PRIVATE_KEY !== undefined ? [process.env.ROOT_PRIVATE_KEY] : [],
+    },
+    rootTest: {
+      url: process.env.ROOT_TEST_RPC || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    child: {
+      url: process.env.CHILD_RPC || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    childTest: {
+      url: process.env.CHILD_TEST_RPC || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    hardhat: {
+      // allow impersonation of smart contracts without modifying balance
+      gasPrice: 0,
+      hardfork: "berlin",
+    },
+  },
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS as unknown as boolean) || false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY !== undefined ? process.env.ETHERSCAN_API_KEY : "",
+      goerli: process.env.ETHERSCAN_API_KEY !== undefined ? process.env.ETHERSCAN_API_KEY : "",
+      polygon: process.env.POLYGONSCAN_API_KEY !== undefined ? process.env.POLYGONSCAN_API_KEY : "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY !== undefined ? process.env.POLYGONSCAN_API_KEY : "",
+    },
+  },
+  mocha: {
+    timeout: 100000000,
+  },
+  dodoc: {
+    // uncomment to stop docs from autogenerating each compile
+    runOnCompile: false,
+  },
+};
+
+export default config;
